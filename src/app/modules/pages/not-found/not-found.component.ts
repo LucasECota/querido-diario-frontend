84563aff2c99b3282484e7cfac23773d052ea890
Lucas Guima <lucasguima.felipe@gmail.com>
2026-09-09 21:41:04 -0300

    feat(pages/not-found): update script

--- src/app/modules/pages/not-found/not-found.component.ts
+++ src/app/modules/pages/not-found/not-found.component.ts
@@ -3,24 +3,20 @@ import { Meta, Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-not-found',
-    templateUrl: './not-found.component.html',
-    styleUrls: ['./not-found.component.sass']
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.sass']
 })
 export class NotFoundComponent implements OnInit {
 
-    constructor(
-        private meta: Meta,
-        private title: Title,
-        private router: Router
-    ) { }
+  constructor(
+    private meta: Meta,
+    private title: Title,
+    private router: Router
+  ) { }
 
-    ngOnInit(): void {
-        this.meta.updateTag({ name: 'robots', content: 'noindex, nofollow' });
-        this.title.setTitle('Página não encontrada - Querido Diário');
-    }
-
-    goHome(): void {
-        this.router.navigate(['/']);
-    }
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'robots', content: 'noindex, nofollow' });
+    this.title.setTitle('Página não encontrada - Querido Diário');
+  }
 }
